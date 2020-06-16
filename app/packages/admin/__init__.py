64a0d6a5d11f5643ee95bba7f9146b60bd0ca70c
pd-shah <pd.shahsafi@gmail.com>
2020-06-16 15:17:10 +0430

    user-admin done

--- app/packages/admin/__init__.py
+++ app/packages/admin/__init__.py
@@ -4,7 +4,12 @@ from flask import (
 )
 from flask_login import login_required
 from app.packages.authentication.logics import permission_required
-from app.packages.authentication.models import Permission
+from app.packages.authentication.models import (
+    Permission,
+    User,
+    Role,
+    Image,
+)
 
 bp = Blueprint(
     "admin",
@@ -20,3 +25,16 @@ bp = Blueprint(
 @permission_required(Permission.ADMIN)
 def index():
     return render_template('admin/index.html')
+
+
+@bp.route("/user-admin")
+@login_required
+@permission_required(Permission.ADMIN)
+def user_admin():
+    user_attributes = User.__table__.columns._data.keys()
+    users = User.query.all()
+    return render_template(
+        'admin/user.html',
+        user_attributes=user_attributes,
+        users=users,
+    )
